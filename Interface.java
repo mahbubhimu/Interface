44c997f9528fdaf45d5489d670634b6957986f9f
Your Name <you@example.com>
2018-10-10 22:48:18 +0600

    Interface Vs Abstract Class

---
+++ Interface.java
@@ -0,0 +1,43 @@
+//Write a program to add 2+4+6+......+20
+abstract class DemoAbstract{
+    abstract void printSum();
+    void printPrime(){
+        int num=15,flag=1;
+        for (int i = 2; i < num; i++) {
+            if(num%i==0){
+                flag=0;
+                break;
+            }
+        }
+        if(flag==1)
+            System.out.println("Prime");
+        else
+            System.out.println("Not Prime");
+    }
+}
+class AbstImplementation extends DemoAbstract{
+    int sum;
+    @Override
+    void printSum(){
+        for (int i = 2; i <= 20; i=i+2) {
+            sum +=i;
+        }
+        System.out.println(sum);
+    }
+}
+
+public class JavaApplication3{
+    public static void main(String[] args){
+        AbstImplementation obj = new AbstImplementation();
+        obj.printSum();
+        obj.printPrime();
+    }
+}
+
+//An Abstract Class can have both
+    //Abstract Method and Non abstract method
+    //Abstract method has no definition but only declaration
+    //The child class must Override the abstract method
+//Abstract class is extended not Implementated
+//Interface is Implemented not Extended
+//Interface can have only have Abstract method 
