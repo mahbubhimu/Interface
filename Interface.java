eb1211f1f27ac474d08a1a2fb3e9967cb12b5d29
Mahbubur Rahman Chowdhury <mahbubhimu@gmail.com>
2018-10-27 10:39:16 +0600

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -33,11 +33,5 @@ public class JavaApplication3{
         obj.printPrime();
     }
 }
+//Abstract Class starts with abstract keyword.
 
-//An Abstract Class can have both
-    //Abstract Method and Non abstract method
-    //Abstract method has no definition but only declaration
-    //The child class must Override the abstract method
-//Abstract class is extended not Implementated
-//Interface is Implemented not Extended
-//Interface can have only have Abstract method 
